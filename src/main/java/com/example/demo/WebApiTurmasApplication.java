0a7b3c69fbd76832a0efe7a9a5a4484810262d9d
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-11-23 14:47:11 -0300

    first commit

---
+++ src/main/java/com/example/demo/WebApiTurmasApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiTurmasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiTurmasApplication.class, args);
+	}
+
+}
